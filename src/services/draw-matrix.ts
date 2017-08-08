e26329a720dc13e09d79d80e017f96267fca3b9d
Duncan Luk <mail.d.luk@gmail.com>
2017-08-08 17:19:33 +0200

    Added drawEmptyTile setting

--- src/services/draw-matrix.ts
+++ src/services/draw-matrix.ts
@@ -1,9 +1,12 @@
 import Matrix from '../interfaces/matrix';
 import IPanel from '../models/panel';
+import settings from '../settings';
 
 export default function drawMatrix(panel: IPanel, matrix: Matrix, tileSize: number): void {
     matrix.forEach((col, x) => col.forEach((item, y) => {
-        if (typeof item === 'undefined') return;
+        if (typeof item === 'undefined'
+            || !settings.drawEmptyTiles && !item) return;
+
         panel.ctx.fillStyle = item ? '#000' : '#8ED6FF';
         panel.ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
     }));
---
+++ src/settings.ts
@@ -0,0 +1,5 @@
+const settings = {
+    drawEmptyTiles: false
+};
+
+export default settings;
