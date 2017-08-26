31becf740faeac395dc593778459fee48003d535
Duncan Luk <mail.d.luk@gmail.com>
2017-08-26 13:29:03 +0200

    Changed board size

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -5,6 +5,6 @@ import Player from '../models/player';
 export const player = new Player({ x: 7, y: 0 });
 export const panel = new Panel('game');
 export const board = new Board({
-    width: 16,
-    height: 24
+    width: 10,
+    height: 20
 });
