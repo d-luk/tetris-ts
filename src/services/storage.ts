2dffd9dde26a90ace5fe6b1e8efff4c59f045e19
Duncan Luk <mail.d.luk@gmail.com>
2017-08-26 13:42:54 +0200

    Starting position based on board size

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -2,9 +2,11 @@ import Panel from '../components/panel';
 import Board from '../models/board';
 import Player from '../models/player';
 
-export const player = new Player({ x: 7, y: 0 });
-export const panel = new Panel('game');
-export const board = new Board({
+const boardSize = {
     width: 10,
     height: 20
-});
+};
+
+export const player = new Player({ x: boardSize.width / 2 - 1, y: 0 });
+export const panel = new Panel('game');
+export const board = new Board(boardSize);
